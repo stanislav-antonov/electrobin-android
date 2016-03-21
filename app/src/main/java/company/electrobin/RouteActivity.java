516b4e2ea2ffe01ad1018ca2b7cc121f5a286f1c
Stanislav Mosienko <stanislav.dev@gmail.com>
2016-03-21 04:06:51 +0300

    * map loading * interacting with location provider

--- app/src/main/java/company/electrobin/RouteActivity.java
+++ app/src/main/java/company/electrobin/RouteActivity.java
@@ -1,51 +1,75 @@
 package company.electrobin;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import company.electrobin.i10n.I10n;
 import company.electrobin.user.User;
 
 public class RouteActivity extends AppCompatActivity {
 
+    private ElectrobinApplication mApp;
     private User mUser;
+    private I10n mI10n;
+
     private Button mBtn;
-    private WebView mVwMap;
+    private WebView mWvMap;
+
+    private RelativeLayout mRlMain;
+    private RelativeLayout mRlLoading;
+    private RelativeLayout mRlLoadRetry;
+
+    private boolean mIsMapLoading;
+
+    private Handler mHandler = new Handler();
+    private MapLoadBreaker mMapLoadBreaker;
 
     private final static String LOG_TAG = RouteActivity.class.getSimpleName();
+    private final static int TIMEOUT_MAP_LOAD = 30000;
 
     private class MyLocationListener implements LocationListener {
 
         private Location mCurrentLocation;
+        private static final int LOCATION_EXPIRES_TIME_INTERVAL = 1000 * 60 * 2;
 
         @Override
         public synchronized void onLocationChanged(Location location) {
+            // Check the new location fix
+            if (isBetterLocation(location, mCurrentLocation))
+                mCurrentLocation = location;
 
-            double lat = location.getLatitude();
-            double lng = location.getLongitude();
+            double lat = mCurrentLocation.getLatitude();
+            double lng = mCurrentLocation.getLongitude();
 
-            double accuracy = location.getAccuracy();
-            String provider = location.getProvider();
+            double accuracy = mCurrentLocation.getAccuracy();
+            String provider = mCurrentLocation.getProvider();
 
-            Log.d(LOG_TAG, String.format("coords: [%1$s, %2$s], provider: %3$s, accuracy: %4$s",
-                    lat, lng, provider, accuracy));
+            Log.d(LOG_TAG, String.format("coords: [%1$s, %2$s], provider: %3$s, accuracy: %4$s, bearing: %5$s",
+                    lat, lng, provider, accuracy, mCurrentLocation.getBearing()));
 
             String strJs = String.format("javascript:updatePosition(%1$s, %2$s)", lat, lng);
-            mVwMap.loadUrl(strJs);
+            mWvMap.loadUrl(strJs);
         }
 
         @Override
@@ -56,25 +80,145 @@ public class RouteActivity extends AppCompatActivity {
 
         @Override
         public void onProviderDisabled(String provider) {}
+
+        /** Determines whether one Location reading is better than the current Location fix
+         * @param location  The new Location that you want to evaluate
+         * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+         */
+        protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+            if (currentBestLocation == null)
+                // A new location is always better than no location
+                return true;
+
+            // Check whether the new location fix is newer or older
+            long timeDelta = location.getTime() - currentBestLocation.getTime();
+            boolean isSignificantlyNewer = timeDelta > LOCATION_EXPIRES_TIME_INTERVAL;
+            boolean isSignificantlyOlder = timeDelta < -LOCATION_EXPIRES_TIME_INTERVAL;
+            boolean isNewer = timeDelta > 0;
+
+            // If it's been more than two minutes since the current location, use the new location
+            // because the user has likely moved
+            if (isSignificantlyNewer) {
+                return true;
+                // If the new location is more than two minutes older, it must be worse
+            }
+            else if (isSignificantlyOlder) {
+                return false;
+            }
+
+            // Check whether the new location fix is more or less accurate
+            int accuracyDelta = (int)(location.getAccuracy() - currentBestLocation.getAccuracy());
+            boolean isLessAccurate = accuracyDelta > 0;
+            boolean isMoreAccurate = accuracyDelta < 0;
+            boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+            // Check if the old and new location are from the same provider
+            boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                    currentBestLocation.getProvider());
+
+            // Determine location quality using a combination of timeliness and accuracy
+            if (isMoreAccurate) {
+                return true;
+            }
+            else if (isNewer && !isLessAccurate) {
+                return true;
+            }
+            else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         *
+         * @param provider1
+         * @param provider2
+         * @return
+         */
+        private boolean isSameProvider(String provider1, String provider2) {
+            if (provider1 == null)
+                return provider2 == null;
+
+            return provider1.equals(provider2);
+        }
     }
 
     private class MyWebViewClient extends WebViewClient {
+
         @Override
-        public void onPageFinished(WebView view, String url){
+        public void onPageFinished(WebView view, String url) {
             LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
             MyLocationListener locationListener = new MyLocationListener();
 
             try {
                 locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000L, 0F, locationListener);
-            } catch(IllegalArgumentException e) {
+            }
+            catch(IllegalArgumentException e) {
                 Log.e(LOG_TAG, "Network provider error: " + e.getMessage());
             }
 
             try {
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000L, 0F, locationListener);
-            } catch (IllegalArgumentException e) {
+            }
+            catch (IllegalArgumentException e) {
                 Log.e(LOG_TAG, "GPS provider error: " + e.getMessage());
             }
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Log.d(LOG_TAG, String.format("Error: %1$s, %2$s, %3$s", errorCode, description, failingUrl));
+        }
+    }
+
+    private class WebAppInterface {
+
+        private Context mContext;
+
+        public WebAppInterface(Context context) {
+            mContext = context;
+        }
+
+        @JavascriptInterface
+        public void onMapReady() {
+            // Mark the map is not loading anymore
+            mIsMapLoading = false;
+
+            // Important: it must go before we make mRlLoading invisible
+            if (mMapLoadBreaker != null) mHandler.removeCallbacks(mMapLoadBreaker);
+
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mRlLoading.setVisibility(View.GONE);
+                }
+            });
+        }
+    }
+
+    private class MapLoadBreaker implements Runnable  {
+        @Override
+        public void run() {
+            // Mark the map is not loading anymore
+            mIsMapLoading = false;
+            mWvMap.stopLoading();
+
+            mRlLoading.setVisibility(View.GONE);
+            mRlLoadRetry.setVisibility(View.VISIBLE);
+
+            Button btnLoadRetry = (Button)mRlLoadRetry.findViewById(R.id.load_retry_button);
+            btnLoadRetry.setText(mI10n.l("retry"));
+            btnLoadRetry.setOnClickListener(new MapLoadRetryHandler());
+        }
+    }
+
+    private class MapLoadRetryHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            mRlLoadRetry.setVisibility(View.GONE);
+            mRlLoading.setVisibility(View.VISIBLE);
+            loadMap();
         }
     }
 
@@ -83,13 +227,32 @@ public class RouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
 
-        mUser = ((ElectrobinApplication)getApplicationContext()).getUser();
+        mApp = (ElectrobinApplication)getApplicationContext();
+        mUser = mApp.getUser();
+        mI10n = mApp.getI10n();
+
         mBtn = (Button)findViewById(R.id.jump_button);
 
-        mVwMap = (WebView)findViewById(R.id.map);
-        WebSettings webSettings = mVwMap.getSettings();
+        mRlMain = (RelativeLayout)findViewById(R.id.main_layout);
+
+        mRlLoading = (RelativeLayout)findViewById(R.id.loading_layout);
+        mRlLoading.setVisibility(View.VISIBLE);
+
+        mRlLoadRetry = (RelativeLayout)findViewById(R.id.load_retry_layout);
+        mRlLoadRetry.setVisibility(View.GONE);
+
+        mWvMap = (WebView)findViewById(R.id.map);
+
+        WebSettings webSettings = mWvMap.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        mWvMap.addJavascriptInterface(new WebAppInterface(this), "Android");
+        mWvMap.setWebViewClient(new MyWebViewClient());
+
+        loadMap();
+    }
+
+    private void loadMap() {
         try {
             InputStream is = getAssets().open("map.html");
             byte[] buffer = new byte[is.available()];
@@ -97,7 +260,10 @@ public class RouteActivity extends AppCompatActivity {
             is.close();
 
             String htmlText = new String(buffer);
-            mVwMap.loadDataWithBaseURL(
+
+            if (mIsMapLoading) mWvMap.stopLoading();
+
+            mWvMap.loadDataWithBaseURL(
                     "http://ru.yandex.api.yandexmapswebviewexample.ymapapp",
                     htmlText,
                     "text/html",
@@ -106,12 +272,17 @@ public class RouteActivity extends AppCompatActivity {
             );
         }
         catch (IOException e) {
-            e.printStackTrace();
+            Log.e(LOG_TAG, e.getMessage());
+            throw new IllegalArgumentException(e.getMessage());
         }
 
-        mVwMap.setWebViewClient(new MyWebViewClient());
-    }
+        mIsMapLoading = true;
 
+        if (mMapLoadBreaker != null) mHandler.removeCallbacks(mMapLoadBreaker);
+
+        mMapLoadBreaker = new MapLoadBreaker();
+        mHandler.postDelayed(mMapLoadBreaker, TIMEOUT_MAP_LOAD);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
