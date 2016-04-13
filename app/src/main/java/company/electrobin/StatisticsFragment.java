f4a704a724f8c5a7812c9b74f6d4a8482d654242
Stanislav Mosienko <stanislav.dev@gmail.com>
2016-04-14 01:46:12 +0300

    * dialog, location bearing, scrollview

--- app/src/main/java/company/electrobin/StatisticsFragment.java
+++ app/src/main/java/company/electrobin/StatisticsFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import company.electrobin.i10n.I10n;
@@ -19,7 +19,7 @@ public class StatisticsFragment extends Fragment {
     private I10n mI10n;
     private ElectrobinApplication mApp;
 
-    private RelativeLayout mRlCommon;
+    private LinearLayout mLlCommon;
     private Button btnNewRoute;
 
     private OnFragmentInteractionListener mListener;
@@ -49,7 +49,7 @@ public class StatisticsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_statistics, container, false);
-        mRlCommon = (RelativeLayout)view.findViewById(R.id.common_layout);
+        mLlCommon = (LinearLayout)view.findViewById(R.id.common_layout);
         return view;
     }
 
@@ -71,13 +71,13 @@ public class StatisticsFragment extends Fragment {
     }
 
     public void showUI() {
-        ((TextView)mRlCommon.findViewById(R.id.header_text)).setText(mI10n.l("statistics"));
+        ((TextView) mLlCommon.findViewById(R.id.header_text)).setText(mI10n.l("statistics"));
 
         final RouteActivity.Route route = mListener.onGetRoute();
         if (route != null)
-            ((TextView) mRlCommon.findViewById(R.id.run_text)).setText(String.format(mI10n.l("run"), route.getRunFormatted()));
+            ((TextView) mLlCommon.findViewById(R.id.run_text)).setText(String.format(mI10n.l("run"), route.getRunFormatted()));
 
-        btnNewRoute = (Button)mRlCommon.findViewById(R.id.new_route_button);
+        btnNewRoute = (Button) mLlCommon.findViewById(R.id.new_route_button);
         btnNewRoute.setText(mI10n.l("get_route"));
         btnNewRoute.setOnClickListener(new View.OnClickListener() {
             @Override
