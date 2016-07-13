1a2bdd906506d3b4c40fc288cf08fd9e12db8ba6
Stanislav Mosienko <stanislav.dev@gmail.com>
2016-07-13 14:48:45 +0300

    * gcm

--- app/src/main/java/company/electrobin/common/Constants.java
+++ app/src/main/java/company/electrobin/common/Constants.java
@@ -4,7 +4,4 @@ public class Constants {
     public static final String REST_API_BASE_URL = "https://185.118.64.121/v1.02";
     public static final String SOCKET_API_HOST = "185.118.64.121";
     public static final int SOCKET_API_PORT = 4321;
-
-    public static final String GCM_SENT_TOKEN_TO_SERVER = "gcmSentTokenToServer";
-    public static final String GCM_REGISTRATION_COMPLETE = "gcmRegistrationComplete";
 }
