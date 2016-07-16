abdd4473fc181d762ea079e58261629c70a88f31
Stanislav Mosienko <stanislav.dev@gmail.com>
2016-07-16 17:14:13 +0300

    * gcm

--- app/src/main/java/company/electrobin/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/company/electrobin/fcm/MyFirebaseMessagingService.java
@@ -25,14 +25,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
-        sendNotification(remoteMessage.getNotification().getBody());
+        String body = "";
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        if (notification != null) {
+            body = remoteMessage.getNotification().getBody();
+        }
+
+        Log.d(TAG, "Notification Message Body: " + body);
+
+        sendNotification(body);
     }
 
     /**
